b9c610563d34c1f4c96af44acb2ecc50fa0d502f
Nikolaus Piccolotto <prayerslayer@gmail.com>
2013-08-23 22:12:05 +0200

    relative values!

--- assistance-prototype/libs/views/AssistanceRectangleAnnotationView.js
+++ assistance-prototype/libs/views/AssistanceRectangleAnnotationView.js
@@ -27,10 +27,10 @@ var assistance = assistance || {};
 			// see http://stackoverflow.com/questions/9651167/svg-not-rendering-properly-as-a-backbone-view
 			this.setElement( document.createElementNS( "http://www.w3.org/2000/svg", this.tagName ) );
 			// the line above also destroys the template ;_;
-			d3.select( this.el ).attr( "x", this.model.get( "x1" ) );
-			d3.select( this.el ).attr( "y", this.model.get( "y1" ) );
-			d3.select( this.el ).attr( "width", this.model.get("x2" ) - this.model.get( "x1" ) );
-			d3.select( this.el ).attr( "height", this.model.get( "y2" ) - this.model.get( "y1" ) );
+			d3.select( this.el ).attr( "x", this.model.get( "x1" ) + "%" );
+			d3.select( this.el ).attr( "y", this.model.get( "y1" ) + "%" );
+			d3.select( this.el ).attr( "width", this.model.get("x2" ) - this.model.get( "x1" ) + "%" );
+			d3.select( this.el ).attr( "height", this.model.get( "y2" ) - this.model.get( "y1" ) + "%" );
 			this.$el = $( this.el );
 		}
 
--- assistance-prototype/libs/views/AssistanceTextAnnotationView.js
+++ assistance-prototype/libs/views/AssistanceTextAnnotationView.js
@@ -20,8 +20,8 @@ var assistance = assistance || {};
 			// see http://stackoverflow.com/questions/9651167/svg-not-rendering-properly-as-a-backbone-view
 			this.setElement( document.createElementNS( "http://www.w3.org/2000/svg", this.tagName ) );
 			// the line above also destroys the template ;_;
-			d3.select( this.el ).attr( "x", this.model.get( "x" ) );
-			d3.select( this.el ).attr( "y", this.model.get( "y" ) );
+			d3.select( this.el ).attr( "x", this.model.get( "x" ) + "%" );
+			d3.select( this.el ).attr( "y", this.model.get( "y" ) + "%");
 			d3.select( this.el ).text( this.model.get( "text" ) );
 			d3.select( this.el ).attr( "class", "assistance-comment__textannotation" );
 			this.$el = $( this.el );
