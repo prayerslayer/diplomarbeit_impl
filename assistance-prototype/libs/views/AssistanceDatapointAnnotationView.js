bf4e97b260969d38f78f09979859071601660204
Nikolaus Piccolotto <prayerslayer@gmail.com>
2013-08-28 08:55:25 +0200

    show datapoint annotations

--- assistance-prototype/libs/views/AssistanceDatapointAnnotationView.js
+++ assistance-prototype/libs/views/AssistanceDatapointAnnotationView.js
@@ -14,7 +14,18 @@ var assistance = assistance || {};
 		className: "assistance-comment__datapointannotation",
 		template: "#pointannotationViewTemplate",
 
-		
+		show: function() {
+			var dings = d3.select( this.model.get( "component" ) ).select( "[resource=" + this.model.get( "uri" ) + "]" );
+			dings.attr( "data-vizboard-old-class", dings.attr( "class" ) );
+			dings.attr( "class", "vizboard-highlight");
+		},
+
+		hide: function() {
+			var dings = d3.select( this.model.get( "component" ) ).select( "[resource=" + this.model.get( "uri" ) + "]" );
+			dings
+				.attr( "class", dings.attr( "data-vizboard-old-class" ) || dings.attr( "class" ) )
+				.attr( "data-vizboard-old-class", null );
+		}
 
 	});
 })( jQuery );
